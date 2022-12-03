c4a2e29abf9d3cdc5aa7083650746ce933a8f246
Miles Dame <milesedame@gmail.com>
2022-12-03 16:45:03 -0500

    Add comments

--- src/preprocessor/TriangleIdentifier.java
+++ src/preprocessor/TriangleIdentifier.java
@@ -48,23 +48,45 @@ public class TriangleIdentifier
 		
 		List<Segment> segmentList;
 		
+		// Loop through all of the segments
 		for (i = 0; i < segmentArr.length; i++) {
+			// Get the first segment
 			Segment seg1 = (Segment) segmentArr[i];
 			
+			// Loop through all of the remaining segments
 			for (int j = (i + 1); j < segmentArr.length; j++) {
+				// Get the next segment
 				Segment seg2 = (Segment) segmentArr[j];
 				
+				// Check if the second segment shares a vertex with the first segment 
 				if (seg1.hasSharedVertex(seg2)) {
+					
+					// If so then get that shared point
 					sharedPoint = seg1.sharedVertex(seg2);
+					
+					// Get the other points from each segment (the two that don't match between the segments)
 					point1 = seg1.other(sharedPoint);
 					point2 = seg2.other(sharedPoint);
+					
+					// Create a segment from those two points to use for finding a third segment
 					Segment matchMaker = new Segment(point1, point2);	
 					
+					// Loop through the remaining segments after the point where the second segment is found
 					for (int k = (j + 1); j < segmentArr.length; k++) {
+						
+						// Check if the current segment matches the matchMaker segment
 						if (segmentArr[k].equals(matchMaker)) {
+							
+							// If it does then get it
 							Segment seg3 = (Segment) segmentArr[k];
+							
+							// Create a list of the three matching segments
 							segmentList = createSegList(seg1, seg2, seg3);
+							
+							// Create a triangle from the list
 							Triangle tri = new Triangle(segmentList);
+							
+							// Add the triangle to the database
 							_triangles.add(tri);
 						}
 					}
